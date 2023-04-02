35381f5588bb408a9a6bf6df963b692fa0cd952e
Jennifer <jennifertorchelsen@gmail.com>
2023-04-02 20:00:28 -0300

    Criando Menu funcional

--- avenidaBet.py
+++ avenidaBet.py
@@ -1,3 +1,15 @@
+while (True):
+
+    print()
+    print("-"*30)
+    print("1. Cadastrar Aposta")
+    print("2. Listar Aposta")
+    print("3. Listar Resultado")
+    print("4. Total de Aposta")
+    print("5. Aposta por Resultado")
+    print("6. Resultado e Premiação")
+    print("7. Premiação")
+
 
 
 
